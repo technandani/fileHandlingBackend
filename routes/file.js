a36a1bc29f4bda6e8752dbe189e3eb238d5b98bf
Nandani <nks854338@gmail.com>
2024-09-24 04:55:55 +0530

    commited

--- routes/file.js
+++ routes/file.js
@@ -1,32 +1,40 @@
 const express = require('express');
-const fs = require('fs');
+const fs = require('fs').promises; 
 const path = require('path');
 const tasks = require('../data.json');
-const jwt = require('jsonwebtoken');
 const { authenticateToken } = require('../middleware/auth');
 const router = express.Router();
 
 const dataFilePath = path.join(__dirname, '..', 'data.json');
 
-router.post('/save',authenticateToken, async(req, res) => {
+router.post('/save', authenticateToken, async (req, res) => {
   const task = req.body;
-  tasks.push({ ...task, QuoteId: tasks.length + 1 });
 
-  await fs.writeFile(dataFilePath, JSON.stringify(tasks, null, 2), (error) => {
-    if (error) {
-      return res.status(500).json({ message: 'Failed to save data', error });
-    }
+  try {
+    tasks.push({ ...task, QuoteId: tasks.length + 1 });
+
+    await fs.writeFile(dataFilePath, JSON.stringify(tasks, null, 2));
+
+    console.log(tasks); 
     return res.status(200).json({ message: 'Data saved successfully' });
-  });
-  console.log(tasks);
+  } catch (error) {
+    console.error('Error saving data:', error);
+    return res.status(500).json({ message: 'Failed to save data', error });
+  }
 });
 
-router.get('/read',authenticateToken ,(req, res) => {
-  if (fs.existsSync(dataFilePath)) {
-    const data = fs.readFileSync(dataFilePath, 'utf8');
-    return res.status(200).json(JSON.parse(data));
-  } else {
-    return res.status(404).json({ message: 'No data found' });
+router.get('/read', authenticateToken, async (req, res) => {
+  try {
+    if (await fs.stat(dataFilePath)) {
+      const data = await fs.readFile(dataFilePath, 'utf8');
+      return res.status(200).json(JSON.parse(data));
+    }
+  } catch (error) {
+    if (error.code === 'ENOENT') {
+      return res.status(404).json({ message: 'No data found' });
+    }
+    console.error('Error reading data:', error);
+    return res.status(500).json({ message: 'Failed to read data', error });
   }
 });
 
