1add516ec29df080578d27160935b8daab55be0c
Nandani <nks854338@gmail.com>
2024-09-24 05:11:37 +0530

    c

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ mongoose
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors({
-  origin: 'https://file-handling-plum.vercel.app', 
+  origin: ' http://localhost:5173', 
   methods: 'GET, POST',
   credentials: true,
 }));
